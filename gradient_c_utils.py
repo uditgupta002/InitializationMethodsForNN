4800942164acb262a425664c3bd1c08342dc9920
Udit Gupta <32953246+uditgupta002@users.noreply.github.com>
2017-11-08 00:53:45 +0530

    Create gradient_c_utils.py

---
+++ gradient_c_utils.py
@@ -0,0 +1,306 @@
+'''
+Created on Nov 7, 2017
+
+@author: udit.gupta
+'''
+
+import numpy as np
+import scipy.io
+
+def load_2D_dataset():
+    data = scipy.io.loadmat('../datasets/data.mat')
+    train_X = data['X'].T
+    train_Y = data['y'].T
+    test_X = data['Xval'].T
+    test_Y = data['yval'].T
+
+    
+    return train_X, train_Y, test_X, test_Y
+
+
+def sigmoid(x):
+    """
+    Compute the sigmoid of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(x)
+    """
+    s = 1/(1+np.exp(-x))
+    return s
+
+def relu(x):
+    """
+    Compute the relu of x
+
+    Arguments:
+    x -- A scalar or numpy array of any size.
+
+    Return:
+    s -- relu(x)
+    """
+    s = np.maximum(0,x)
+    
+    return s
+
+def initialize_parameters(layer_dims):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the dimensions of each layer in our network
+    
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    W1 -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
+                    b1 -- bias vector of shape (layer_dims[l], 1)
+                    Wl -- weight matrix of shape (layer_dims[l-1], layer_dims[l])
+                    bl -- bias vector of shape (1, layer_dims[l])
+                    
+    Tips:
+    - For example: the layer_dims for the "Planar Data classification model" would have been [2,2,1]. 
+    This means W1's shape was (2,2), b1 was (1,2), W2 was (2,1) and b2 was (1,1). Now you have to generalize it!
+    - In the for loop, use parameters['W' + str(l)] to access Wl, where l is the iterative integer.
+    """
+    
+    np.random.seed(3)
+    parameters = {}
+    L = len(layer_dims) # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) / np.sqrt(layer_dims[l-1])
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+        print('Shape of W' + str(l) + ' is '+ str(parameters['W' + str(l)].shape))
+        print('Shape of b' + str(l) + ' is '+ str(parameters['b' + str(l)].shape))
+        assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
+        assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+        
+    return parameters
+
+def update_parameters(parameters, grads, learning_rate):
+    """
+    Update parameters using gradient descent
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters:
+                    parameters['W' + str(i)] = Wi
+                    parameters['b' + str(i)] = bi
+    grads -- python dictionary containing your gradients for each parameters:
+                    grads['dW' + str(i)] = dWi
+                    grads['db' + str(i)] = dbi
+    learning_rate -- the learning rate, scalar.
+    
+    Returns:
+    parameters -- python dictionary containing your updated parameters 
+    """
+    
+    n = len(parameters) // 2 # number of layers in the neural networks
+
+    # Update rule for each parameter
+    for k in range(n):
+        parameters["W" + str(k+1)] = parameters["W" + str(k+1)] - learning_rate * grads["dW" + str(k+1)]
+        parameters["b" + str(k+1)] = parameters["b" + str(k+1)] - learning_rate * grads["db" + str(k+1)]
+        
+    return parameters
+
+
+def compute_cost(a3, Y):
+    """
+    Implement the cost function
+    
+    Arguments:
+    a3 -- post-activation, output of forward propagation
+    Y -- "true" labels vector, same shape as a3
+    
+    Returns:
+    cost - value of the cost function
+    """
+    m = Y.shape[1]
+    
+    logprobs = np.multiply(-np.log(a3),Y) + np.multiply(-np.log(1 - a3), 1 - Y)
+    cost = 1./m * np.nansum(logprobs)
+
+def model(X, Y, learning_rate = 0.3, num_iterations = 30000, print_cost = True):
+    """
+    Implements a three-layer neural network: LINEAR->RELU->LINEAR->RELU->LINEAR->SIGMOID.
+    
+    Arguments:
+    X -- input data, of shape (input size, number of examples)
+    Y -- true "label" vector (1 for blue dot / 0 for red dot), of shape (output size, number of examples)
+    learning_rate -- learning rate of the optimization
+    num_iterations -- number of iterations of the optimization loop
+    print_cost -- If True, print the cost every 10000 iterations
+    lambd -- regularization hyperparameter, scalar
+    keep_prob - probability of keeping a neuron active during drop-out, scalar.
+    
+    Returns:
+    parameters -- parameters learned by the model. They can then be used to predict.
+    """
+        
+    grads = {}
+    costs = []                            # to keep track of the cost
+    m = X.shape[1]                        # number of examples
+    layers_dims = [X.shape[0], 20, 3, 1]
+    
+    # Initialize parameters dictionary.
+    parameters = initialize_parameters(layers_dims)
+
+    # Loop (gradient descent)
+
+    for i in range(0, num_iterations):
+
+        # Forward propagation: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID.
+        a3, cache = forward_propagation(X, parameters)
+        
+        # Cost function
+        cost = compute_cost(a3, Y)
+            
+        # Backward propagation.
+        grads = backward_propagation(X, Y, cache)
+        
+        # Update parameters.
+        parameters = update_parameters(parameters, grads, learning_rate)
+        
+        # Print the loss every 10000 iterations
+        if print_cost and i % 10000 == 0:
+            print("Cost after iteration {}: {}".format(i, cost))
+        if print_cost and i % 1000 == 0:
+            costs.append(cost)
+    
+    return parameters
+
+def forward_propagation(X, Y, parameters):
+    """
+    Implements the forward propagation (and computes the cost) presented in Figure 3.
+    
+    Arguments:
+    X -- training set for m examples
+    Y -- labels for m examples 
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3":
+                    W1 -- weight matrix of shape (5, 4)
+                    b1 -- bias vector of shape (5, 1)
+                    W2 -- weight matrix of shape (3, 5)
+                    b2 -- bias vector of shape (3, 1)
+                    W3 -- weight matrix of shape (1, 3)
+                    b3 -- bias vector of shape (1, 1)
+    
+    Returns:
+    cost -- the cost function (logistic cost for one example)
+    """
+    
+    # retrieve parameters
+    m = X.shape[1]
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+
+    # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+    Z1 = np.dot(W1, X) + b1
+    A1 = relu(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = relu(Z2)
+    Z3 = np.dot(W3, A2) + b3
+    A3 = sigmoid(Z3)
+
+    # Cost
+    logprobs = np.multiply(-np.log(A3),Y) + np.multiply(-np.log(1 - A3), 1 - Y)
+    cost = 1./m * np.sum(logprobs)
+    
+    cache = (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3)
+    
+    return cost, cache
+
+
+
+def backward_propagation(X, Y, cache):
+    """
+    Implement the backward propagation presented in figure 2.
+    
+    Arguments:
+    X -- input datapoint, of shape (input size, 1)
+    Y -- true "label"
+    cache -- cache output from forward_propagation_n()
+    
+    Returns:
+    gradients -- A dictionary with the gradients of the cost with respect to each parameter, activation and pre-activation variables.
+    """
+    
+    m = X.shape[1]
+    (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3) = cache
+    
+    dZ3 = A3 - Y
+    dW3 = 1./m * np.dot(dZ3, A2.T)
+    db3 = 1./m * np.sum(dZ3, axis=1, keepdims = True)
+    
+    dA2 = np.dot(W3.T, dZ3)
+    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+    dW2 = 1./m * np.dot(dZ2, A1.T)
+    db2 = 1./m * np.sum(dZ2, axis=1, keepdims = True)
+    
+    dA1 = np.dot(W2.T, dZ2)
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dW1 = 1./m * np.dot(dZ1, X.T)
+    db1 = 1./m * np.sum(dZ1, axis=1, keepdims = True)
+    
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,
+                 "dA2": dA2, "dZ2": dZ2, "dW2": dW2, "db2": db2,
+                 "dA1": dA1, "dZ1": dZ1, "dW1": dW1, "db1": db1}
+    
+    return gradients
+
+
+def dictionary_to_vector(parameters):
+    """
+    Roll all our parameters dictionary into a single vector satisfying our specific required shape.
+    """
+    keys = []
+    count = 0
+    for key in ["W1", "b1", "W2", "b2", "W3", "b3"]:
+        
+        # flatten parameter
+        new_vector = np.reshape(parameters[key], (-1,1))
+        keys = keys + [key]*new_vector.shape[0]
+        
+        if count == 0:
+            theta = new_vector
+        else:
+            theta = np.concatenate((theta, new_vector), axis=0)
+        count = count + 1
+
+    return theta, keys
+
+def vector_to_dictionary(theta):
+    """
+    Unroll all our parameters dictionary from a single vector satisfying our specific required shape.
+    """
+    parameters = {}
+    parameters["W1"] = theta[:40].reshape((20,2))
+    parameters["b1"] = theta[40:60].reshape((20,1))
+    parameters["W2"] = theta[60:120].reshape((3,20))
+    parameters["b2"] = theta[120:123].reshape((3,1))
+    parameters["W3"] = theta[123:126].reshape((1,3))
+    parameters["b3"] = theta[126:127].reshape((1,1))
+
+    return parameters
+
+def gradients_to_vector(gradients):
+    """
+    Roll all our gradients dictionary into a single vector satisfying our specific required shape.
+    """
+    
+    count = 0
+    for key in ["dW1", "db1", "dW2", "db2", "dW3", "db3"]:
+        # flatten parameter
+        new_vector = np.reshape(gradients[key], (-1,1))
+        
+        if count == 0:
+            theta = new_vector
+        else:
+            theta = np.concatenate((theta, new_vector), axis=0)
+        count = count + 1
+
+    return theta
